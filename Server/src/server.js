386364dbc889e9f3981953c4ea5670d59d68f3c8
huynhhothoty <20110597@student.hcmute.edu.vn>
2023-10-14 21:18:20 +0700

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,39 +1,42 @@
-const express = require('express')
-const cors = require('cors')
-const db = require('./configs/DBconfig')
-const rootRouter = require('./routers/index')
-const CustomError = require('./utils/CustomError')
-const ErrorHandler = require('./middlewares/Error/ErrorHandler')
-
+const express = require('express');
+const cors = require('cors');
+const db = require('./configs/DBconfig');
+const rootRouter = require('./routers/index');
+const CustomError = require('./utils/CustomError');
+const ErrorHandler = require('./middlewares/Error/ErrorHandler');
 
 // Init
-const dotenv = require('dotenv')
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 3000
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
 
 // connect database
-db.connect()
+db.connect();
 
 // json format
-app.use(express.json())
+app.use(express.json());
 
 // Setting cors
-app.use(cors())
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // set route
-app.use('/api', rootRouter)
+app.use('/api', rootRouter);
 
 // invalid route
 app.all('*', (req, res, next) => {
-    next(new CustomError(`Can't find this route`, 404))
-})
+    next(new CustomError(`Can't find this route`, 404));
+});
 
 // middleware use to handle error
-app.use(ErrorHandler)
-
+app.use(ErrorHandler);
 
 // listening
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+    console.log(`App listening at http://localhost:${port}`);
+});
