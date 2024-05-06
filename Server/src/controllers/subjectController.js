212cbd5b9118a3f71eb6aff64a947b00408cf7aa
huynhhothoty <20110597@student.hcmute.edu.vn>
2024-05-06 15:42:19 +0700

    Update subjectController.js

--- Server/src/controllers/subjectController.js
+++ Server/src/controllers/subjectController.js
@@ -1,12 +1,59 @@
 const Subject = require('../models/subjectModel');
 const crudFactory = require('./crudFactory');
 const CustomError = require('../utils/CustomError');
+const ApiFeatures = require('../utils/ApiFeature');
+const Major = require('../models/majorModel');
 
 //
 const createSubject = crudFactory.createOne(Subject);
 const updateSubject = crudFactory.updateOne(Subject);
 const getSubject = crudFactory.getOne(Subject, 'branchMajor');
-const getAllSubject = crudFactory.getAll(Subject);
+const getAllSubject = async (req, res, next) => {
+    try {
+        const { majorId, ...otherQuery } = req.query;
+
+        let departmentChildOfMajor;
+        let databaseQuery, databaseQuery2;
+        if (majorId) {
+            const major = await Major.findById(majorId).populate('department').lean();
+            if (!major) return next(new CustomError('No major with this id', 404));
+
+            departmentChildOfMajor = major.department.reduce((acc, cur) => {
+                acc = [...acc, ...cur.list];
+                return acc;
+            }, []);
+
+            const departChildIdList = departmentChildOfMajor.map((ele) => ele._id);
+
+            databaseQuery = Subject.find({
+                departmentChild: { $in: departChildIdList },
+            }).lean();
+            databaseQuery2 = Subject.find({
+                departmentChild: { $in: departChildIdList },
+            }).lean();
+        } else {
+            databaseQuery = Subject.find().lean();
+            databaseQuery2 = Subject.find().lean();
+        }
+
+        const apiFeat = new ApiFeatures(databaseQuery, otherQuery);
+        const apiFeat2 = new ApiFeatures(databaseQuery2, otherQuery);
+        apiFeat.filter().sorting().pagination();
+        apiFeat2.filter();
+
+        const docs = await apiFeat.myQuery;
+        const docsOnlyFilter = await apiFeat2.myQuery;
+
+        res.status(200).send({
+            status: 'ok',
+            total: docsOnlyFilter.length,
+            data: docs,
+        });
+    } catch (error) {
+        console.log(error);
+        return next(new CustomError(error));
+    }
+};
 const deleteSubject = crudFactory.deleteOne(Subject);
 const updateManySubject = crudFactory.updateMany(Subject);
 
