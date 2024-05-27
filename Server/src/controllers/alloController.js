324918539c1332c911540c91178f7d925089e86d
huynhhothoty <20110597@student.hcmute.edu.vn>
2024-05-27 17:01:50 +0700

    Update alloController.js

--- Server/src/controllers/alloController.js
+++ Server/src/controllers/alloController.js
@@ -1,5 +1,4 @@
 const Allocation = require('../models/allocationModel');
-const CustomError = require('../utils/CustomError');
 const crudFactory = require('./crudFactory');
 //
 
@@ -11,60 +10,7 @@ const getAllo = crudFactory.getOne(Allocation, [
     { path: 'detail.electiveSubjectList.branchMajor' },
 ]);
 const deleteAllo = crudFactory.deleteOne(Allocation);
-const updateAllo = async (req, res, next) => {
-    try {
-        let updateDoc = await Allocation.findById(req.params.id).lean();
-        const updateInfo = req.body;
-
-        if (!updateDoc) return next(new CustomError('No document with this id', 404));
-        if (Object.keys(updateInfo).includes('detail')) {
-            let temp = updateInfo.detail.map((big, index) => {
-                if (index > updateDoc.detail.length - 1) return big;
-
-                let newSubList = big.subjectList;
-                let oldSubList = updateDoc.detail[index].subjectList;
-                let newAddSub = [];
-
-                newSubList.forEach((sub) => {
-                    if (sub?.isDelete) {
-                        oldSubList = oldSubList.filter(
-                            (x) => x._id.toString() !== sub._id.toString()
-                        );
-                    } else {
-                        if (sub?._id)
-                            oldSubList = oldSubList.map((oldSub) => {
-                                if (oldSub._id.toString() === sub._id) return sub;
-                                return oldSub;
-                            });
-                        else newAddSub.push(sub);
-                    }
-                });
-                oldSubList = [...oldSubList, ...newAddSub];
-                updateInfo.detail[index].subjectList = oldSubList;
-
-                return updateInfo.detail[index];
-            });
-            // if (updateInfo.detail.length > updateDoc.detail.length) {
-            //     for (let i = updateDoc.detail.length; i < updateInfo.detail.length; i++) {
-            //         temp = [...temp, updateInfo.detail[i]];
-            //     }
-            // }
-            updateInfo.detail = temp;
-        }
-        const doc = await Allocation.findByIdAndUpdate(req.params.id, updateInfo, {
-            new: true,
-            runValidators: true,
-        });
-
-        res.status(200).send({
-            status: 'ok',
-            data: doc,
-        });
-    } catch (error) {
-        console.log(error);
-        next(new CustomError(error));
-    }
-};
+const updateAllo = crudFactory.updateOne(Allocation);
 
 //
 module.exports = {
