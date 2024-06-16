94e077fd9a63de88cc2fd7001b5bc38fcf01d8b9
huynhhothoty <20110597@student.hcmute.edu.vn>
2024-06-16 16:11:37 +0700

    Update branchMajorModel.js

--- Server/src/models/branchMajorModel.js
+++ Server/src/models/branchMajorModel.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose');
 
-const branchSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'BranchMajor must have name'],
-        unique: [true, 'BranchMajor must be unique value'],
+const branchSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'BranchMajor must have name'],
+            unique: [true, 'BranchMajor must be unique value'],
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+        branchCode: {
+            type: String,
+            required: [true, 'BranchMajor must have Branchmajor code'],
+            unique: [true, 'BranchMajor code must be unique'],
+        },
+        summary: {
+            type: String,
+            default: 'This is a description of a Branchmajor, you can custom this field',
+        },
     },
-    isActive: {
-        type: Boolean,
-        default: true,
-    },
-    branchCode: {
-        type: String,
-        required: [true, 'BranchMajor must have Branchmajor code'],
-        unique: [true, 'BranchMajor code must be unique'],
-    },
-    summary: {
-        type: String,
-        default: 'This is a description of a Branchmajor, you can custom this field',
-    },
-});
+    { timestamps: true }
+);
 
 branchSchema.pre(/^find/, function () {
     this.select('-__v');
